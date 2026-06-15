58d56a7c71df986196d7911d40d394ed78893d65
ortabyss-gif <ortabyss@hotmail.com>
2026-06-14 20:55:41 -0600

    Conectar proyecto local

---
+++ IniciarProyecto.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "C:\Users\Orta\Documents\MeshMusic"
+
+powershell -NoExit -Command "npm run dev -- --host"
---
+++ android/app/src/main/java/com/jOS/Music/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jOS.Music;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
